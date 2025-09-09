1f95c650ce189bcb8c1ea89a1544a23e196ba0f5
Samuel Rios <srios050402@gmail.com>
2025-09-08 17:49:02 -0700

    complete

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(function() {
-  // your code here
-})
+  $(".accordion-header").on("click", function () {
+    $(".accordion-content").slideUp();
+    $(this).next(".accordion-content").slideDown();
+  });
+
+  $(".todos button").on("click", function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        const todos = data.todos;
+        const ul = $(".todos ul");
+        ul.empty(); 
+        todos.forEach(todo => {
+          ul.append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function () {
+        alert("Error al cargar los datos.");
+      }
+    });
+  });
+});
